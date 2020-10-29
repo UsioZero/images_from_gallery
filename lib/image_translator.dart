e6266e19683f7a3bc8976dc781f3d51026765f8b
Oleg <swortaartonlineforever@gmail.com>
2020-10-29 17:14:15 +0200

    finall

--- lib/image_translator.dart
+++ lib/image_translator.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 
@@ -10,6 +11,8 @@ class ImageTranslator extends StatefulWidget {
 
 class _ImageTranslatorState extends State<ImageTranslator> {
   List<AssetEntity> picturesList = [];
+  int pageNow = 0;
+  int pageLast;
 
   @override
   void initState() {
@@ -17,16 +20,30 @@ class _ImageTranslatorState extends State<ImageTranslator> {
     _fetchImages();
   }
 
+  _onScrollDown(ScrollNotification scroll) {
+    if (scroll.metrics.pixels / scroll.metrics.maxScrollExtent > 0.33) {
+      if (pageNow != pageLast) {
+        _fetchImages();
+      }
+    }
+  }
+
   _fetchImages() async {
+    pageLast = pageNow;
     var result = await PhotoManager.requestPermission();
     if (result) {
       List<AssetPathEntity> albums =
           await PhotoManager.getAssetPathList(onlyAll: true);
-
-      List<AssetEntity> pictures = await albums[0].getAssetListPaged(0, 20);
-      setState(() {
-        picturesList = pictures;
-      });
+      print(albums);
+      List<AssetEntity> pictures =
+          await albums[0].getAssetListPaged(pageNow, 60);
+      print(pictures.length);
+      if (pictures.length != 0)
+        setState(() {
+          picturesList.addAll(pictures);
+          pageNow++;
+          print(pageNow);
+        });
     }
   }
 
@@ -42,24 +59,56 @@ class _ImageTranslatorState extends State<ImageTranslator> {
           leading: Icon(Icons.image),
         ),
       ),
-      body: GridView.builder(
-        itemCount: picturesList.length,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (context, index) {
-          return FutureBuilder(
-            future: picturesList[index].thumbData,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done)
-                return Image.memory(
-                  snapshot.data,
-                  fit: BoxFit.cover,
-                );
-              return Container();
-            },
-          );
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (notification) {
+          _onScrollDown(notification);
+          return;
         },
+        child: GridView.builder(
+          itemCount: picturesList.length,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (context, index) {
+            return FutureBuilder(
+              future: picturesList[index].thumbData,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done)
+                  return FlatButton(
+                    child: Image.memory(snapshot.data),
+                    onPressed: () {
+                      _onButtonPressed(
+                          context, snapshot, picturesList[index].title);
+                    },
+                  );
+                return Container();
+              },
+            );
+          },
+        ),
       ),
+    );
+  }
+
+  Future _onButtonPressed(
+      BuildContext context, AsyncSnapshot snapshot, String title) {
+    return showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Text('Oh sheet, you pressed button'),
+          content: Image.memory(snapshot.data),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              child: Text('Add'),
+              onPressed: () {},
+            ),
+            CupertinoDialogAction(
+              child: Text('Back'),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        );
+      },
     );
   }
 }
